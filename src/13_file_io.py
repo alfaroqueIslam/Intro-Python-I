1a6e41035e0e277f6013b7ced767d3a08b67eb59
Alfaroque Islam <alfaroque5@gmail.com>
2020-09-01 19:01:35 -0400

    CS Unit 1 Assignment 2

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -4,16 +4,21 @@ at how to read and write to files here:
 
 https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 """
-
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
-
-# YOUR CODE HERE
+os.listdir()
+with open('foo.txt') as f:
+    read_data = f.read()
+print(read_data)
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
-# YOUR CODE HERE
+b = open("bar.txt", "w+")
+b.write('Hello \nworld \n!')
+with open('bar.txt') as b:
+    rd = f.read()
+print(rd)
